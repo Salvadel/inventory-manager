fb9ad3e90fe1e13833cd43b49a8a9897b0f51882
Salvadel <salfdeluca@gmail.com>
2026-03-19 14:53:33 -0400

    added functionalities for categories and vendor to database.py and inventory.py

--- src/demo.py
+++ src/demo.py
@@ -79,3 +79,28 @@ def inventory_menu():
 if __name__ == "__main__":
             inventory.startup()
             login_menu()
+
+
+
+'''
+MALCOLM
+
+All the functiosn you use will be in inventory.py
+
+In this section if you can add the ability to assign items to categories and view items by category that would be great. 
+You can add a new menu option for managing categories in the inventory menu, allowing users to create new categories, 
+assign items to categories, and view items by category. these are in:
+new_category(category_name), delete_category(category_name), get_all_categories(), assign_item_to_category(item_id, category_name), 
+unassign_item_from_category(item_id, category_name), and get_all_items_by_category(category_name) in inventory.py
+
+Also if you could add the search item functionality to the inventory menu that would be great, allowing users to 
+search for items by name and view the results. it is in search_inventory(name) in inventory.py
+
+Also the ability to add and remove vendors from items would be great, allowing users to manage their vendor relationships for each inventory item.
+These are in add_vendor_to_item(item_id, vendor_name), remove_vendor_from_item(item_id, vendor_name), and 
+update_vendor_for_item(item_id, old_vendor_name, new_vendor_name) in inventory.py
+
+And lastly to to buy list funcationalites which are located in add_item_to_buy_list(item_id), remove_item_from_to_buy_list(item_id), and view_to_buy_list() in inventory.py
+
+Let me know if you have any questions, all the functions should be operationsal and completed your ojb is just to gather the user inputs and display everything in the CLI.
+'''
